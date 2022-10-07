5690a6ea83a431eeefb7cfc72678c9a178b6ac7a
joaquinreyes2905 <mikemoble2905@gmail.com>
2022-10-06 22:18:11 -0300

    cuadratica de rios

---
+++ cuadratica/src/Test/CuadraticaTest.java
@@ -0,0 +1,19 @@
+package Test;
+
+import cuadratica.Cuadratica;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class CuadraticaTest {
+
+    Cuadratica calculator = new Cuadratica();
+
+    @Test
+    public void cuadraticaTest() {
+
+    }
+
+}
---
+++ cuadratica/src/cuadratica/Cuadratica.java
@@ -0,0 +1,11 @@
+package cuadratica;
+
+public class Cuadratica {
+
+    public static double [] cuadratica(int a, int b, int c) {
+        double x1 = (-b - (Math.sqrt(b * b) - 4 * a * b) / a * 2);
+        double x2 = (-b + (Math.sqrt(b * b) - 4 * a * b) / a * 2);
+
+        return new double[0];
+    }
+}
