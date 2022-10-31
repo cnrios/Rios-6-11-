1062c1fa815c017d1c39b3a5d1f674f360be4066
cnrios <cnrios2015@gmail.com>
2022-10-31 19:05:16 -0300

    nose

--- cuadratica/src/Test/CuadraticaTest.java
+++ cuadratica/src/Test/CuadraticaTest.java
@@ -1,28 +1,20 @@
 package Test;
 
 import cuadratica.Cuadratica;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CuadraticaTest{
     Cuadratica calculator = new Cuadratica();
-
     @Test
-
     public void raizTest() {
-    
-    
-    
-       assertEquals(calculator.raiz(1, 2, -8), -4,2 );
-    
-    
-    
-    }
 
+        assertEquals(calculator.raiz(1, 2, -8), -4,2 );
+
+    }
     private void assertEquals(double raiz, int i, int j) {
+
     }
-    
-    
-    
-    }
+}
--- cuadratica/src/cuadratica/Cuadratica.java
+++ cuadratica/src/cuadratica/Cuadratica.java
@@ -1,13 +1,10 @@
 package cuadratica;
 
 public class Cuadratica {
-
-public double raiz (int a, int b, int c) {
+   public double raiz (int a, int b, int c) {
 
    return  -b+-Math.sqrt((b*b) - (4 * a * c)) / 2 * a;
 
-}
-
-
+   }
 }
 
