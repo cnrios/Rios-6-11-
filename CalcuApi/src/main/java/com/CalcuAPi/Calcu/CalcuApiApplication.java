a44c241eb32f77c99097f427325310a0c2439808
cnrios <84513678+cnrios@users.noreply.github.com>
2022-11-10 22:15:41 -0300

    Me olvide el hola
    nah me olvide le hola mundo

---
+++ CalcuApi/src/main/java/com/CalcuAPi/Calcu/CalcuApiApplication.java
@@ -0,0 +1,21 @@
+package com.CalcuAPi.Calcu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcuApiApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(CalcuApiApplication.class);
+
+	public static void main(String[] args){
+
+		SpringApplication.run(CalcuApiApplication.class , args);
+
+		logger.debug("MI MENSAJE DEBUg");
+
+	}
+
+}
